538f71d9403937151f093806aba2c0c8541449e4
lorraineferreira <lorraineferreira.contato@outlook.com>
2024-10-29 14:59:29 -0300

    Adicionado novos elementos

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value ) 
     const max = Math.floor( document.querySelector(".input-max").value)
-
-    const result = Math.floor(Math.random() *( max - min + 1)) + min
-
     const text = document.querySelector(".text-add")
+    const condition = document.querySelector(".text-condition")
+   
 
-    text.innerHTML = result
+    if(max > min){
+        const result = Math.floor(Math.random() *( max - min + 1)) + min
+        text.innerHTML = result
+
+    } else{
+        condition.innerHTML = "O valor mínimo tem que ser MENOR que o valor máximo"
+    }
+
+   
+
+    
 
 
 }
